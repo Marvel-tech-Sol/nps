76d4bc795478f4d20873ab38d86e368dc751a28c
dharmatejaneralla287 <dharmatejaneralla287@gmail.com>
2023-07-14 17:07:33 +0530

    added error pages

--- nps/settings.py
+++ nps/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&lw0mqe5!si-kp@2mf3&xrq1du6$pf#ixmi@ye+ve9iq$79&n8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
 ALLOWED_HOSTS = ['*']
 
